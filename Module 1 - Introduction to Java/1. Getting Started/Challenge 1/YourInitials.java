fd8b732c5a1d841f61fdee5be1f666bcbbb34f1b
LewisOT <91518540+LewisOT@users.noreply.github.com>
2022-04-28 19:30:32 -0500

    YourInitials completed

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,13 @@
 public class YourInitials {
     public static void main(String[] args) {
-        //print initials here. 
+        // print initials here.
+        System.out.println("V    V    OOO");
+        System.out.println("V    V  O     O");
+        System.out.println("V    V  O     O");
+        System.out.println("V    V  O     O");
+        System.out.println("V    V  O     O");
+        System.out.println(" V  V    O   O");
+        System.out.println("  V       OOO");
+
     }
 }
